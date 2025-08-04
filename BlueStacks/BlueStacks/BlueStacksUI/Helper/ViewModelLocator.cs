0ae07d5eb8b02dd9b10b92e1e4bdf86f9df1084c
de-company Misty <156302636+De-Misty@users.noreply.github.com>
2025-08-04 23:19:44 +0300

    Add files via upload

---
+++ BlueStacks/BlueStacks/BlueStacksUI/Helper/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace BlueStacks.BlueStacksUI.Helper
+{
+	// Token: 0x020002CE RID: 718
+	public class ViewModelLocator
+	{
+		// Token: 0x06001AB8 RID: 6840 RVA: 0x00011D57 File Offset: 0x0000FF57
+		static ViewModelLocator()
+		{
+			SimpleIoc.Default.Register<MinimizeBlueStacksOnCloseView>();
+		}
+	}
+}
